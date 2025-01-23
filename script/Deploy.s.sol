009c5cbf24733615a568717ba5ccf1e378c44ca9
0xdevocado <nikolajzl@hotmail.com>
2025-01-23 19:38:15 +0100

    feat: initial deploy script

--- script/Deploy.s.sol
+++ script/Deploy.s.sol
@@ -1,6 +1,15 @@
 // SPDX-License-Identifier: AGPL-3.0-or-later
 pragma solidity 0.8.21;
 
-import {Script, console2} from "forge-std/Script.sol";
+import {Script, console} from "forge-std/Script.sol";
+import {Rewards} from "../src/Rewards.sol";
 
-contract Deploy is Script {}
+contract Deploy is Script {
+
+    function run() external {
+        vm.startBroadcast();
+        Rewards rewards = new Rewards(msg.sender); //TODO: change this to read admin from a file
+        vm.stopBroadcast();
+        console.log("Rewards deployed at:", address(rewards));
+    }
+}
