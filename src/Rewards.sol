d02f45cf771b49b2fd7a2c7b660935e6e70f5302
0xdevocado <nikolajzl@hotmail.com>
2025-01-28 12:59:31 +0100

    feat: add access control

--- src/Rewards.sol
+++ src/Rewards.sol
@@ -2,44 +2,48 @@
 
 pragma solidity 0.8.21;
 
-import {Address} from "@openzeppelin/contracts/utils/Address.sol";
-import {Ownable} from "@openzeppelin/contracts/access/Ownable.sol";
 import {SafeERC20, IERC20} from "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
 import {MerkleProof} from "@openzeppelin/contracts/utils/cryptography/MerkleProof.sol";
+import {AccessControl} from "@openzeppelin/contracts/access/AccessControl.sol";
 
-contract Rewards is Ownable {
+contract Rewards is AccessControl {
     using SafeERC20 for IERC20;
 
     address public wallet;
     bytes32 public merkleRoot;
 
+    // epoch => isClosed
     mapping(uint256 => bool) public epochClosed;
-    mapping(address => mapping(uint256 => uint256)) public cumulativeClaimed; // account => epoch => amount
+    // account => epoch => amount
+    mapping(address => mapping(uint256 => uint256)) public cumulativeClaimed;
 
-    // This event is triggered whenever a call to #setWallet succeeds.
+    // Roles
+    bytes32 public constant WALLET_ROLE = keccak256("WALLET_ROLE");
+    bytes32 public constant MERKLE_ROOT_ROLE = keccak256("MERKLE_ROOT_ROLE");
+    bytes32 public constant EPOCH_ROLE = keccak256("EPOCH_ROLE");
+
+    // Events
     event WalletUpdated(address oldWallet, address newWallet);
-    // This event is triggered whenever a call to #setMerkleRoot succeeds.
     event MerkelRootUpdated(bytes32 oldMerkleRoot, bytes32 newMerkleRoot);
-    // This event is triggered whenever a call to #claim succeeds.
-    event Claimed(address indexed account, uint256 amount);
-    // This event is triggered whenever a call to #incrementEpoch succeeds.
-    event EpochUpdated(uint256 oldEpoch, uint256 newEpoch);
-    // This event is triggered whenever a call to #setEpochClosed succeeds.
     event EpochIsClosed(uint256 epoch, bool isClosed);
+    event Claimed(address indexed account, uint256 amount);
 
-    constructor() Ownable(msg.sender) {}
+    constructor() {
+        _grantRole(DEFAULT_ADMIN_ROLE, msg.sender);
+    }
 
     /* ========== ADMIN FUNCTIONS ========== */
-    function setWallet(address wallet_) public onlyOwner {
+    function setWallet(address wallet_) public onlyRole(WALLET_ROLE) {
         wallet = wallet_;
+        emit WalletUpdated(wallet, wallet_);
     }
 
-    function setMerkleRoot(bytes32 merkleRoot_) external onlyOwner {
-        emit MerkelRootUpdated(merkleRoot, merkleRoot_);
+    function setMerkleRoot(bytes32 merkleRoot_) external onlyRole(MERKLE_ROOT_ROLE) {
         merkleRoot = merkleRoot_;
+        emit MerkelRootUpdated(merkleRoot, merkleRoot_);
     }
 
-    function setEpochClosed(uint256 epoch, bool isClosed) public onlyOwner {
+    function setEpochClosed(uint256 epoch, bool isClosed) public onlyRole(EPOCH_ROLE) {
         epochClosed[epoch] = isClosed;
         emit EpochIsClosed(epoch, isClosed);
     }
@@ -58,7 +62,6 @@ contract Rewards is Ownable {
         require(!epochClosed[epoch], "Rewards/epoch-not-enabled");
 
         // Construct the leaf
-        // See https://github.com/OpenZeppelin/merkle-tree?tab=readme-ov-file#validating-a-proof-in-solidity for more info
         bytes32 leaf = keccak256(bytes.concat(keccak256(abi.encode(epoch, account, token, cumulativeAmount))));
 
         // Verify the proof
