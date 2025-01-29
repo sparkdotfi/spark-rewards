9a5f4abec242eb5fe5462580de8589c0f059a912
0xdevocado <nikolajzl@hotmail.com>
2025-01-29 18:53:46 +0100

    feat: random tokens and epochs

--- merkle-tree-scripts/generateInput.js
+++ merkle-tree-scripts/generateInput.js
@@ -1,17 +1,28 @@
-// JavaScript version of the script to generate input data
 const fs = require('fs');
 
-// Constants
-const tokenAddress = "0x5615dEB798BB3E4dFa0139dFa1b3D433Cc23b72f";
-const epoch = 1;
+// Change these values as needed
+const tokenAddresses = [
+    "0x5615dEB798BB3E4dFa0139dFa1b3D433Cc23b72f",
+    "0x2e234DAe75C793f67A35089C9d99245E1C58470b"
+];
 const cumulativeMin = BigInt("10000000000000000"); // 0.01 ETH
 const cumulativeMax = BigInt("10000000000000000000000000"); // 10,000 ETH
-const outputFile = "input_data.json";
-const entries = 100000;
+const numberOfEntries = 100; // Set the number of entries to generate
+const outputFilePath = "input_data.json"; // Set output file name
 
 // Function to generate a random Ethereum address
 function generateRandomEthAddress() {
     return "0x" + [...Array(20)].map(() => Math.floor(Math.random() * 256).toString(16).padStart(2, '0')).join('');
+}
+
+// Function to generate a random token address
+function getRandomToken() {
+    return tokenAddresses[Math.floor(Math.random() * tokenAddresses.length)];
+}
+
+// Function to generate a random epoch
+function getRandomEpoch() {
+    return (Math.floor(Math.random() * 5) + 1).toString(); // Random epoch from 1 to 5
 }
 
 // Function to generate the data
@@ -19,9 +30,9 @@ function generateData(entries) {
     const data = [];
     for (let i = 0; i < entries; i++) {
         const entry = {
-            epoch: epoch,
+            epoch: getRandomEpoch(),
             account: generateRandomEthAddress(),
-            token: tokenAddress,
+            token: getRandomToken(),
             cumulativeAmount: (cumulativeMin + BigInt(Math.floor(Math.random() * Number(cumulativeMax - cumulativeMin)))).toString()
         };
         data.push(entry);
@@ -29,10 +40,13 @@ function generateData(entries) {
     return data;
 }
 
-// Generate the data with specified amount of entries
-const data = generateData(entries);
+// Main function
+(async () => {
+    // Generate the data with the specified number of entries
+    const data = generateData(numberOfEntries);
 
-// Write the data to a JSON file
-fs.writeFileSync(outputFile, JSON.stringify(data, null, 4));
+    // Write the data to a JSON file
+    fs.writeFileSync(outputFilePath, JSON.stringify(data, null, 4));
 
-console.log(`Generated ${data.length} entries and saved to ${outputFile}`);
+    console.log(`Generated ${data.length} entries and saved to ${outputFilePath}`);
+})();
