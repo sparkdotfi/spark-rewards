a3571bcd9c6630b860d718a460ba6f99973a83b7
0xdevocado <nikolajzl@hotmail.com>
2025-01-31 14:21:43 +0100

    refactor: remove wallet role and setting merkle root

--- script/Deploy.s.sol
+++ script/Deploy.s.sol
@@ -7,7 +7,6 @@ import {SparkRewards} from "../src/SparkRewards.sol";
 contract Deploy is Script {
     bytes32 public constant EPOCH_ROLE       = keccak256("EPOCH_ROLE");
     bytes32 public constant MERKLE_ROOT_ROLE = keccak256("MERKLE_ROOT_ROLE");
-    bytes32 public constant WALLET_ROLE      = keccak256("WALLET_ROLE");
 
     function run() external {
         // Read the config file
@@ -16,20 +15,14 @@ contract Deploy is Script {
         // Parse the JSON file vars
         address admin = vm.parseJsonAddress(config, ".admin");
         address wallet = vm.parseJsonAddress(config, ".wallet");
-        bytes32 merkleRoot = vm.parseJsonBytes32(config, ".root");
-
         address epochAdmin = vm.parseJsonAddress(config, ".epoch_role");
         address merkleRootAdmin = vm.parseJsonAddress(config, ".merkle_role");
-        address walletAdmin = vm.parseJsonAddress(config, ".wallet_role");
 
         console.log("Deploying SparkRewards contract with the following parameters:");
         console.log("Admin:", admin);
         console.log("Wallet:", wallet);
         console.log("Epoch Admin:", epochAdmin);
         console.log("Merkle Root Admin:", merkleRootAdmin);
-        console.log("Wallet Admin:", walletAdmin);
-        console.log("Merkle Root:");
-        console.logBytes32(merkleRoot);
         vm.startBroadcast();
 
         // Deploy the contract using the parsed admin address
@@ -38,13 +31,9 @@ contract Deploy is Script {
         // Set the wallet using the parsed wallet address
         rewards.setWallet(wallet);
 
-        // Set the merkle root using the parsed merkle root
-        rewards.setMerkleRoot(merkleRoot);
-
         // Grant the roles using the parsed role addresses
         rewards.grantRole(EPOCH_ROLE, epochAdmin);
         rewards.grantRole(MERKLE_ROOT_ROLE, merkleRootAdmin);
-        rewards.grantRole(WALLET_ROLE, walletAdmin);
 
         // Transfer the ownership to the admin
         rewards.grantRole(rewards.DEFAULT_ADMIN_ROLE(), admin);
